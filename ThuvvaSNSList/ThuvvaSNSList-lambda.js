038d022fd40c572759324fa1f3ecc7d18209c6f8
thuvva <35223608+thuvva@users.noreply.github.com>
2018-01-16 15:26:31 +0530

    2nd

--- ThuvvaSNSList/ThuvvaSNSList-lambda.js
+++ ThuvvaSNSList/ThuvvaSNSList-lambda.js
@@ -7,11 +7,15 @@ exports.handler = function (event, context, callback) {
 		TopicArn: 'arn:aws:sns:us-east-1:480964559519:MOCK12'
 	}).promise()
 		.then(data => {
+			console.log(data);
+			callback(null,data);
 			// your code goes here
 		})
 		.catch(err => {
+			console.log(err);
+			callback(null,err);
 			// error handling goes here
 		});
 
-	callback(null, 'Successfully executed');
+	//callback(null, 'Successfully executed');
 }
