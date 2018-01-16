e4b6daaeb2756b94560d0650f079777662f1a6f2
thuvva <35223608+thuvva@users.noreply.github.com>
2018-01-16 15:20:07 +0530

    1st commit

---
+++ ThuvvaSNSList/ThuvvaSNSList-lambda.js
@@ -0,0 +1,17 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
+
+
+	sns.listSubscriptionsByTopic({
+		TopicArn: 'arn:aws:sns:us-east-1:480964559519:MOCK12'
+	}).promise()
+		.then(data => {
+			// your code goes here
+		})
+		.catch(err => {
+			// error handling goes here
+		});
+
+	callback(null, 'Successfully executed');
+}
